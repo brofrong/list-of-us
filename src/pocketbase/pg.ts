09ad585adaf8c15a60cb501f1c8b91b85b5091cf
dga <dima737.99@mail.ru>
2025-04-03 01:08:55 +0300

    add types for pocketbase

--- src/pocketbase/pg.ts
+++ src/pocketbase/pg.ts
@@ -1,6 +1,7 @@
 import PocketBase from "pocketbase";
 import { env } from "~/lib/env";
+import { TypedPocketBase } from "./pocketbase-types";
 
-const pb = new PocketBase(env.VITE_POCKETBASE_URL);
+const pb = new PocketBase(env.VITE_POCKETBASE_URL) as TypedPocketBase;
 
 export default pb;
